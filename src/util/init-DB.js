60a4e1abb0ebeeab038853e51fed7c2576717c17
yael.lotan <yaelotan19@gmail.com>
2020-05-30 10:38:09 +0300

    feat(util/init-DB.js): print the node env for debugging

--- src/util/init-DB.js
+++ src/util/init-DB.js
@@ -6,6 +6,7 @@ const init = () => {
   const isProduction = process.env.NODE_ENV === 'production';
   
   const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+  console.log(`NODE_ENV:  ${process.env.NODE_ENV}`);
   
   const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
